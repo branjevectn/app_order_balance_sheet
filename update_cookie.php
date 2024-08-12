f49a2721c8789c60b6ddf8341d73c029b4927bf6
branjevectn <3105729+branjevectn@users.noreply.github.com>
2024-08-12 14:40:49 +0500

    Update and rename update_cookie.php to update_cookie.go

--- update_cookie.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_COOKIE['total_pagez'])){
-  $total_pages = $_COOKIE['total_pagez'];
-}
-$page = $_POST['page'];
-
-	for($i = 1; $i <= $total_pages; $i++){ ?>
-			<a href="#" class="<?php if($i == $page) { echo 'active'; }?>" data-page="<?php echo $i; ?>"><?php echo $i; ?></a>
-		<?php } ?>
