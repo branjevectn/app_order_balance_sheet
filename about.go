eada2e14a0c45e45dfc223a2ae1768185b00d2f4
Heli. <3105729+heli-diogo@users.noreply.github.com>
2024-09-09 13:27:47 +0500

    Update about.go

--- about.go
+++ about.go
@@ -1,6 +1,6 @@
 package main
 import ("fmt")
-// fxdfgdfhhdfhvbgfgsfhzcgxgjcxfefhvhhfdgxxvtdfgbfgffdgdghhfghdbddgghdgfxfsccvjbvtsvbghdsfvaggbggddhdfhfghfdghfddgfscvhhgsdghjfvhfyffhhdgghgcvxcvvcccvdghgvfddgxcvvghdfgvddsgdsfvbfggfdghaxvvvfsdgffd
+// fxfsggdfgdfhhdfhvbgfgsfhzcgxgjcxfefhvhhfdgxxvtdfgbfgffdgdghhfghdbddgghdgfxfsccvjbvtsvbghdsfvaggbggddhdfhfghfdghfddgfscvhhgsdghjfvhfyffhhdgghgcvxcvvcccvdghgvfddgxcvvghdfgvddsgdsfvbfggfdghaxvvvfsdgffd
 func msfain() {
   x := 10
   y := 5
