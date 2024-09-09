eee37bb7d9ab4fa9d58bd9888ba20ae68676e10b
Heli. <3105729+heli-diogo@users.noreply.github.com>
2024-09-09 12:52:54 +0500

    Update about.go

--- about.go
+++ about.go
@@ -1,6 +1,6 @@
 package main
 import ("fmt")
-// vdcdcxfdfgggscfcxdfhdvdcddggdcvvccfggffghrfgvdcvgfccvcfvcddffccvchbrfcbv
+// vdcdcfdfxfdfgggscfcxdfhdvdcddggdcvvccfggffghrfgvdcvgfccvcfvcddffccvchbrfcbv
 func main() {
   x := 10
   y := 5
